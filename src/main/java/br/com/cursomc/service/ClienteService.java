ad2f34e4f4fe66349b332d0693e1e3b222e92faf
José Robert <robertspfc10@gmail.com>
2019-06-22 02:12:36 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/br/com/cursomc/service/ClienteService.java
+++ src/main/java/br/com/cursomc/service/ClienteService.java
@@ -2,11 +2,19 @@ package br.com.cursomc.service;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.cursomc.domain.Cliente;
+import br.com.cursomc.dto.ClienteDTO;
 import br.com.cursomc.repositories.ClienteRepository;
+import br.com.cursomc.service.exceptions.DataIntegrityException;
 import br.com.cursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -22,5 +30,40 @@ public class ClienteService {
 		+ ", Tipo: " + Cliente.class.getName());
 		}
 		return obj;
+	}
+	
+	public Cliente update(Cliente obj) {
+		Cliente newObj = find(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		
+		try {
+			repo.delete(id);
 		}
+		catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é posssivel excluir porque há entidades relacionadas");
+		}
+	}
+
+	public List<Cliente> findAll() {
+		return repo.findAll();
+	}
+	
+	public Page<Cliente> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = new PageRequest(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
+	
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	private void updateData(Cliente newObj, Cliente obj) {
+		newObj.setNome(obj.getNome());
+		newObj.setEmail(obj.getEmail());
+	}
 }
