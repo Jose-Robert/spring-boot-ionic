ef5e7625514d0719026319b92758a1a64eaf9935
Jose-Robert <robert.suporteweb@gmail.com>
2020-12-19 22:51:59 -0300

    Padronizando formato das exceções

--- src/main/java/br/com/cursomc/resources/exception/FieldMessage.java
+++ src/main/java/br/com/cursomc/resources/exception/FieldMessage.java
@@ -2,37 +2,36 @@ package br.com.cursomc.resources.exception;
 
 import java.io.Serializable;
 
-public class FieldMessage implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	private String fieldName;
-	private String message;
-	
-	public FieldMessage() {
-		
-	}
+public class FieldMessage implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-	public FieldMessage(String fieldName, String message) {
-		super();
-		this.fieldName = fieldName;
-		this.message = message;
-	}
+    private String fieldName;
+    private String message;
 
-	public String getFieldName() {
-		return fieldName;
-	}
+    public FieldMessage() {
 
-	public void setFieldName(String fieldName) {
-		this.fieldName = fieldName;
-	}
+    }
 
-	public String getMessage() {
-		return message;
-	}
+    public FieldMessage(String fieldName, String message) {
+	super();
+	this.fieldName = fieldName;
+	this.message = message;
+    }
 
-	public void setMessage(String message) {
-		this.message = message;
-	}
+    public String getFieldName() {
+	return fieldName;
+    }
 
-	
+    public void setFieldName(String fieldName) {
+	this.fieldName = fieldName;
+    }
+
+    public String getMessage() {
+	return message;
+    }
+
+    public void setMessage(String message) {
+	this.message = message;
+    }
+
 }
--- src/main/java/br/com/cursomc/resources/exception/ValidationError.java
+++ src/main/java/br/com/cursomc/resources/exception/ValidationError.java
@@ -4,23 +4,21 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ValidationError extends StandardError {
-	private static final long serialVersionUID = 1L;
-	
-	private List<FieldMessage> erros = new ArrayList<>();
-	
-	public ValidationError(Integer status, String msg, Long timeStamp) {
-		super(status, msg, timeStamp);
-	}
 
-	public List<FieldMessage> getErros() {
-		return erros;
-	}
+    private static final long serialVersionUID = 5790328474733923171L;
+    
+    private List<FieldMessage> errors = new ArrayList<>();
 
-	public void addError(String fieldName, String message) {
-		erros.add(new FieldMessage(fieldName, message));
-	}
-	
+    public ValidationError(Long timestamp, Integer status, String error, String message, String path) {
+	super(timestamp, status, error, message, path);
+    }
 
-	
+    public List<FieldMessage> getErrors() {
+	return errors;
+    }
+
+    public void addError(String fieldName, String messagem) {
+	errors.add(new FieldMessage(fieldName, messagem));
+    }
 
 }
